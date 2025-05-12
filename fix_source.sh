8a115fa625c3febb5826f8d6ffbdb18db9cf8082
Fedor Kupryakov <pikpoksound@gmail.com>
2025-05-12 15:01:42 +0000

    fix source bash sh

---
+++ fix_source.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source $ZSH/custom/themes/powerlevel10k/powerlevel10k.zsh-theme
+source $ZSH/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $ZSH/custom/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
